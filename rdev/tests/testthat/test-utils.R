3e03fd4f63b45325d89177c820868b9082bd5b97
Devin Pastoor <devin.pastoor@gmail.com>
2016-04-20 21:54:12 -0400

    get unexported function exposed to test suite

--- rdev/tests/testthat/test-utils.R
+++ rdev/tests/testthat/test-utils.R
@@ -1,18 +1,18 @@
 context("util functions")
 
 test_that("columns are renamed", {
-  expect_equal(names(rename_cols(Theoph, c("time" = "Time", "dv" = "conc"))),
+  expect_equal(names(mrgsolve:::rename_cols(Theoph, c("time" = "Time", "dv" = "conc"))),
                 c("Subject", "Wt", "Dose", "time", "dv"))
                 })
 
 test_that("columns are renamed and order preserved", {
-  expect_equal(names(rename_cols(Theoph, c("dv" = "conc", "time" = "Time"))),
+  expect_equal(names(mrgsolve:::rename_cols(Theoph, c("dv" = "conc", "time" = "Time"))),
                 c("Subject", "Wt", "Dose", "time", "dv"))
                 })
 
 test_that("columns that don't exist throw an error", {
-  expect_error(rename_cols(Theoph, c("dv" = "Donc")),
+  expect_error(mrgsolve:::rename_cols(Theoph, c("dv" = "Donc")),
               "the following columns do not exist in the dataset:  Donc")
-  expect_error(rename_cols(Theoph, c("dv" = "Donc", "id" = "subject")),
+  expect_error(mrgsolve:::rename_cols(Theoph, c("dv" = "Donc", "id" = "subject")),
                "the following columns do not exist in the dataset:  Donc, subject")
 })
