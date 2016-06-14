e15f1c2cc28de2ee3080e62bfcba087935e92048
kylebmetrum <kyleb@metrumrg.com>
2016-06-14 00:14:39 -0500

    show.output.on.console=FALSE if windows

--- rdev/R/modspec.R
+++ rdev/R/modspec.R
@@ -491,13 +491,19 @@ mread <- function(model=character(0),project=getwd(),code=NULL,udll=TRUE,
   purge_model(cfile(x))
   
   ## Compile the model
-  status <- system(paste0("R CMD SHLIB ",
-                          ifelse(preclean, " --preclean ", ""),
-                          build_path(cfile),
-                          " -o ",
-                          sodll(x,short=TRUE)),
-                   ignore.stdout=ignore.stdout)
+  sys.args <- list(command = paste0("R CMD SHLIB ",
+                                    ifelse(preclean, " --preclean ", ""),
+                                    build_path(cfile),
+                                    " -o ",
+                                    sodll(x,short=TRUE)),
+                   ignore.stdout = ignore.stdout,
+                   show.output.on.console = FALSE)
   
+  if(.Platform$OS.type!="windows") {
+    sys.args[["show.output.on.console"]] <- NULL
+  }
+  
+  status <- do.call(system,sys.args)
   
   file.remove(compfile(package,project(x)))
   
