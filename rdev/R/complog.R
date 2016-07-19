9a0e66b82ff2624b2ab50b7ae2e371dfa90dcc20
kylebmetrum <kyleb@metrumrg.com>
2016-07-19 12:11:33 -0500

    force wait time to be in seconds

--- rdev/R/complog.R
+++ rdev/R/complog.R
@@ -26,7 +26,7 @@ safe_wait <- function(x) {
   target <- compout(model(x),soloc(x))
   if(!file.exists(target)) return(invisible(NULL))
   mt <- file.info(target)[["mtime"]]
-  age <- as.POSIXct(Sys.time()) - as.POSIXct(mt)
+  age <- as.numeric(as.POSIXct(Sys.time())) - as.numeric(as.POSIXct(mt))
   if(age > SAFE_WAIT_TIME) return(invisible(NULL))
   message("(waiting) ...")
   return(Sys.sleep(SAFE_WAIT_TIME-age))
