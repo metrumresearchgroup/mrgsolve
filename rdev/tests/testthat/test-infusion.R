f1952ee9648d27c687a119177835c90829fd2004
kylebmetrum <kyleb@metrumrg.com>
2016-08-04 23:57:44 -0500

    merge test15 with test-infusion

--- rdev/tests/testthat/test-infusion.R
+++ rdev/tests/testthat/test-infusion.R
@@ -82,3 +82,35 @@ test_that("Infusion with duration a multiple of ii", {
     expect_is(out, "mrgsims")
 })
 
+
+
+context("Infusions that end too close to observations.")
+
+mod  <- mrgsolve:::house()
+
+test_that("Infusion with no obs overlap", {
+  mod <- mod %>% ev(amt=6, rate=5)
+  expect_is(mod %>% mrgsim, "mrgsims")
+})
+
+test_that("Infusion with obs overlap", {
+  mod <- mod %>% ev(amt=6, rate=5)
+  expect_is(mod %>% mrgsim(delta=0.1), "mrgsims")
+  #expect_error(mod %>% update(mindt=0) %>% mrgsim(delta=0.1))
+})
+
+
+context("Infusion with ss flag")
+
+test_that("Infusion executes with ss flag and ii==dur", {
+  out <- 
+    mod %>% 
+    Req(CP) %>% obsonly %>%
+    ev(amt=100,rate=100,ii=1,addl=10,ss=1) %>% 
+    mrgsim(end=10,digits=5) %>% filter(time>0)
+  
+  expect_true(all(out$CP==100))
+  
+})
+
+
