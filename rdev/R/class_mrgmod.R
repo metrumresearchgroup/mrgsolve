3321cec2f801b0fee50265b0de0a6f64c9e0a12c
kylebmetrum <kyleb@metrumrg.com>
2016-10-14 13:52:37 -0500

    move remaining contents from methods.R to class_mrgmod.R

--- rdev/R/class_mrgmod.R
+++ rdev/R/class_mrgmod.R
@@ -442,3 +442,30 @@ blocks_ <- function(file,what) {
   cat(paste0(x1,unlist(bl)), sep="\n\n")
 }
 
+
+parin <- function(x) {
+  list(rtol=x@rtol,atol=x@atol, hmin=as.double(x@hmin), hmax=as.double(x@hmax),ixpr=x@ixpr,
+       maxsteps=as.integer(x@maxsteps),mxhnil=x@mxhnil,verbose=as.integer(x@verbose),debug=x@debug,
+       digits=x@digits, tscale=x@tscale,
+       mindt=x@mindt, advan=x@advan)
+}
+
+
+##' Show model specification and C++ files.
+##' 
+##' @param x model object
+##' @param spec logical; show the model specification file
+##' @param source logical; show the C++ file that is actually compiled
+##' @param ... not used
+##' @export
+##' 
+file_show <- function(x,spec=TRUE,source=TRUE,...) {
+  stopifnot(is.mrgmod(x))
+  what <- list()
+  if(spec) what$spec <- cfile(x)
+  if(source) what$source <- x@shlib$source
+  do.call(base::file.show,what)
+}
+
+
+
