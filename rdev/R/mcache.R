9200c84b9dd5625c75e8c4140d3b92bec8fe1bfd
kylebmetrum <kyleb@metrumrg.com>
2016-10-18 23:56:00 -0500

    only test md5 if present in cached object

--- rdev/R/mcache.R
+++ rdev/R/mcache.R
@@ -6,7 +6,7 @@ mread_cache <- function(model,project=getwd(),
                         code=NULL, soloc=tempdir(),quiet=FALSE,...) {
   
   build <- new_build(model,project,soloc,code) 
-
+  
   cache_file <- file.path(build$soloc,"mrgmod_cache.RDS")
   
   ## If the cache file doesn't exist, build and return
@@ -16,8 +16,9 @@ mread_cache <- function(model,project=getwd(),
   if(te) {
     x <- readRDS(cache_file)
     t0 <- is.mrgmod(x)
-    t1 <- is.character(x@shlib$md5)
-    t2 <- x@shlib$md5 == build$md5
+    if(t1 <- is.character(x@shlib$md5)) {
+      t2 <- x@shlib$md5 == build$md5
+    }
     t3 <- file.exists(sodll(x))
   }
   
@@ -30,7 +31,7 @@ mread_cache <- function(model,project=getwd(),
   x <- mread( model,project,soloc=soloc,quiet=quiet,...)
   
   saveRDS(x,file=cache_file)
-
+  
   return(x) 
 }
 
