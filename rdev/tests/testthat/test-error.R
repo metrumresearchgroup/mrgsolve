4d3a893fcb590e3a38a7759fade0d34b984a33ea
kylebmetrum <kyleb@metrumrg.com>
2016-11-05 00:33:54 -0500

    push in test-error

--- rdev/tests/testthat/test-error.R
+++ rdev/tests/testthat/test-error.R
@@ -1,39 +1,41 @@
-library(mrgsolve)
 library(testthat)
-
-
+library(mrgsolve)
+library(dplyr)
 Sys.setenv(R_TESTS="")
 options("mrgsolve_mread_quiet"=TRUE)
 
-project <- file.path(system.file(package="mrgsolve"), "models")
-
-mod <- mread("pk1cmt", modlib())
-
-context("Testing error generation ")
-
-test_that("Error when parameters and compartments have same name", {
-  expect_error(mrgmod("firstmodel", project, param=list(CENT=2)))
-  expect_error(mrgmod("firstmodel", project, init=list(CL=2)))
-})
-
-test_that("Error when parameters and compartments have same name introduced via update", {
-  expect_error(update(mod, param=list(CENT=2), open=TRUE))
-  expect_error(update(mod, init=list(CL=2), open=TRUE))
-})
-
-test_that("Error when a parameter name is listed in compartments", {
-  expect_error(update(mod, init=list(CL=2), open=TRUE))
-  expect_error(mrgmod("firstmodel", project, init=list(CL=2)))
-})
-
-test_that("Error  when a compartment names is listed in parameters", {
-  expect_error(update(mod, param=list(CENT=2), open=TRUE))
-  expect_error(mrgmod("firstmodel", project, param=list(CENT=2)))
-})
 
 
-test_that("Error when the number of parameters changes without recompile",{
-  bad <- update(mod, param=list(FAKEPARAMETER=22), open=TRUE)
-  expect_error(mrgsim(bad))
-})
-
+# 
+# project <- file.path(system.file(package="mrgsolve"), "models")
+# 
+# mod <- mread("firstmodel", project)
+# 
+# context("Testing error generation ")
+# 
+# test_that("Error when parameters and compartments have same name", {
+#   expect_error(mrgmod("firstmodel", project, param=list(CENT=2)))
+#   expect_error(mrgmod("firstmodel", project, init=list(CL=2)))
+# })
+# 
+# test_that("Error when parameters and compartments have same name introduced via update", {
+#   expect_error(update(mod, param=list(CENT=2), open=TRUE))
+#   expect_error(update(mod, init=list(CL=2), open=TRUE))
+# })
+# 
+# test_that("Error when a parameter name is listed in compartments", {
+#   expect_error(update(mod, init=list(CL=2), open=TRUE))
+#   expect_error(mrgmod("firstmodel", project, init=list(CL=2)))
+# })
+# 
+# test_that("Error  when a compartment names is listed in parameters", {
+#   expect_error(update(mod, param=list(CENT=2), open=TRUE))
+#   expect_error(mrgmod("firstmodel", project, param=list(CENT=2)))
+# })
+# 
+# 
+# test_that("Error when the number of parameters changes without recompile",{
+#   bad <- update(mod, param=list(FAKEPARAMETER=22), open=TRUE)
+#   expect_error(mrgsim(bad))
+# })
+# 
