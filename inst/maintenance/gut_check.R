c42c411386a1c26be25ef0e9af046a8624ad13ad
Kyle Baron <kyleb@metrumrg.com>
2017-03-18 11:07:34 -0400

    fix bracket parse error #214

--- inst/maintenance/gut_check.R
+++ inst/maintenance/gut_check.R
@@ -3,3 +3,7 @@ pkg <- satellite_package(all_tests=TRUE)
 keepdir <- file.path("Rchecks", "gut_check")
 if(!dir.exists(keepdir)) dir.create(keepdir)
 devtools::check(basename(pkg),check_dir=keepdir)
+
+
+
+
