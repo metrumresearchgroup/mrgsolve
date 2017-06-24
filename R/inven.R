e910f8630e230c7f134feb77d82b6e52f51a2a8d
Kyle Baron <kyleb@metrumrg.com>
2017-06-24 09:18:13 -0500

    minor formatting and edit inventory details

--- R/inven.R
+++ R/inven.R
@@ -1,43 +1,8 @@
-##' Inventory parameters in R objects.
-##' 
-##' @param x model object
-##' @param obj the object to inventory
-##' @param need required parameters
-##' @param crump logical; if \code{TRUE} an error is generated when any parameter
-##' in \code{need} is not found in the object
-##' 
-##' @export
-inven <- function(x,obj,need=NULL,crump=TRUE) {
-  # if(substr(need,1,1)=="@") {
-  #   if(need=="@all") {
-  #     need <- names(param(x))
-  #   } else {
-  #     need <- get(substr(need,2,nchar(need)),env_get(x,tolist=FALSE))  
-  #   }
-  # }
-  
-  if(is.null(need)) {
-    # return a consistent type of empty character vector 
-    # if nothing missing
-    return(vector(mode = "character"))
-  }
-  
-  need <- cvec_cs(need)
-  
-  if(!all(need %in% names(param(x)))) {
-    stop("need can only be parameter names", call.=FALSE) 
-  }
-  
-  if(crump) {
-    inven_stop(obj,need)
-  }
-  
-  return(inven_report(obj, need))
-}
+
 
 #' Check whether all required parameters needed in a model are present in an object
 #' @param x model object
-#' @param obj dataframe to pass to \code{\link{idata_set}} or \code{\link{data_set}}
+#' @param obj data.frame to pass to \code{\link{idata_set}} or \code{\link{data_set}}
 #' @param ... capture dplyr-style parameter requirements
 #' @param .strict whether to stop execution if all requirements are present (\code{TRUE}) 
 #' or just warn (\code{FALSE}); see details
@@ -51,7 +16,7 @@ inven <- function(x,obj,need=NULL,crump=TRUE) {
 #' @details 
 #' If parameter requirements are not explicitly stated, the requirement defaults to 
 #' all parameter names in \code{x}.  Note that, by default,
-#' the inventory is not \code{.strict} by default unless the user explicitly
+#' the inventory is not \code{.strict} unless the user explicitly
 #' states the parameter requirement. That is, if parameter requirements are explicitly 
 #' stated, \code{.strict} will be set to \code{TRUE} if a value \code{.strict} was not
 #' passed in the call.
@@ -68,11 +33,10 @@ inventory <- function(x,obj,..., .strict = FALSE) {
     if(missing(.strict)) .strict <- TRUE 
   }
   
-  if (.strict) {
-    inven_stop(obj, need, oname)
-  }
+  if(.strict) inven_stop(obj, need, oname)
   
   found <- inven_report(obj, need, oname)
+  
   if (!length(found)) {
     message("Found all required parameters in ", sQuote(oname),".")
   }
@@ -82,18 +46,21 @@ inventory <- function(x,obj,..., .strict = FALSE) {
 
 inven_stop <- function(obj,need,oname) {
   miss <- setdiff(need,names(obj))
+  
   if (length(miss)) {
     stop("missing parameters in ", sQuote(oname), "\n", 
-         paste(paste0(" - ",miss,collapse="\n")),call.=FALSE)
+         paste(paste0(" - ", miss, collapse="\n")), call.=FALSE)
   }
+  
   return(TRUE)
 }
 
 inven_report <- function(obj,need,oname) {
   miss <- setdiff(need,names(obj))
+  
   if (length(miss)) {
     warning("Missing parameters in ", shQuote(oname), "\n", 
-            paste(paste0(" - ",miss,collapse="\n")), call.=FALSE)
+            paste(paste0(" - ", miss, collapse="\n")), call.=FALSE)
   }
   return(miss)
 }
