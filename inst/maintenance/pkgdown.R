d9536bcb819b1502c547dc5fa6c910a942289d6b
Kyle Baron <kyleb@metrumrg.com>
2018-02-17 12:06:56 -0600

    adding maintenance script

---
+++ inst/maintenance/pkgdown.R
@@ -0,0 +1,8 @@
+library(pkgdown)
+library(methods)
+build_site(".", path = "DOCS")
+# init_site(".", path = "DOCS")
+# build_articles(".", path = "DOCS")
+# build_home(".", path = "DOCS")
+# build_reference(".", path = "DOCS")
+
