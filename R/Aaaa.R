5100c2051f3137b0414c4a85a0596fef86601c99
Kyle Baron <kyleb@metrumrg.com>
2018-03-08 08:42:54 -0600

    Stop using UQS; git diff! instead

--- R/Aaaa.R
+++ R/Aaaa.R
@@ -33,7 +33,7 @@
 ##' @importFrom Rcpp evalCpp
 ##' @importFrom RcppArmadillo armadillo_version
 ##' @importFrom tibble as_data_frame data_frame
-##' @importFrom rlang UQS enquo syms
+##' @importFrom rlang enquo syms !!!
 ##' 
 ##' @include class_mrgsims.R
 ##' @include class_mrgmod.R
