7cb57c370fb247815d2d9601d15ee724d4777914
Kyle Baron <kyleb@metrumrg.com>
2019-01-05 08:20:23 -0600

    modify modlib to pass to mread_cache

--- inst/models/emax.cpp
+++ inst/models/emax.cpp
@@ -1,20 +1,21 @@
 $PARAM
-CL=1, VC=10, KA1=0.5, KA2=0.5
-Q=0, VP=10, VMAX=0, KM=2
+CL=1, V2=10, KA=0.5, KA2=0.5
+Q=0, V3=10, VMAX=0, KM=2
 E0=0, EMAX=10, EC50=30, n=1
 
 $CMT EV1 CENT PERIPH EV2
 
 $GLOBAL
-#define CP (CENT/VC)
-#define CT (PERIPH/VP)
+#define CP (CENT/V2)
+#define CT (PERIPH/V3)
 #define CLNL (VMAX/(KM+CP))
 #define RESP (E0+EMAX*pow(CP,n)/(pow(EC50,n)+pow(CP,n)))
 
 $ODE
-dxdt_EV1 = -KA1*EV1;
+dxdt_EV1 = -KA *EV1;
 dxdt_EV2 = -KA2*EV2;
-dxdt_CENT = KA1*EV1 + KA2*EV2 - (CL+CLNL+Q)*CP  + Q*CT;
+dxdt_CENT = KA *EV1 + KA2*EV2 - (CL+CLNL+Q)*CP  + Q*CT;
 dxdt_PERIPH = Q*CP - Q*CT;
 
 $CAPTURE CP RESP
+  
