82de0dd3dc47566a64ef6fd262478b1f702b02e2
Kyle Baron <kyleb@metrumrg.com>
2019-09-02 19:07:17 -0500

    tweak test for 02

--- tests/testthat/test-tad.R
+++ tests/testthat/test-tad.R
@@ -48,7 +48,7 @@ test_that("tad", {
   expect_equal(out$tad,c(-3,-2,-1,0,1,2,0,1,2,0,1,2))
 })
 
-test_that("tad recognizes evid 1 and 4", {
+test_that("tad recognizes evid 1 and 4 issue-502", {
   d1 <- ev(amt = 100, time = 12, evid = 4, addl=2, ii = 4)
   d2 <- ev(amt = 100, time = 12, addl = 2, ii = 4)
   out1 <- mrgsim(mod,d1,tad=TRUE)
