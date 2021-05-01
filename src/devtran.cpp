65299e4d67cffd7e8cea40d2de28b1fb8411278e
Kyle Baron <kyleb@metrumrg.com>
2021-05-01 13:06:59 -0500

    revise code to remove infusions when compartment is turned off

--- src/devtran.cpp
+++ src/devtran.cpp
@@ -610,11 +610,14 @@ Rcpp::List DEVTRAN(const Rcpp::List parin,
         this_rec->implement(&prob);
         if(this_rec->cmt() < 0 && prob.infusion_count[this_cmtn] > 0) {
           int n_inf = prob.infusion_count[this_cmtn];
-          for(int ii = j; ii < a[i].size() && n_inf > 0; ++ii) {
+          int n_end = a[i].size();
+          for(int ii = j; (n_inf > 0 && ii < n_end); ++ii) {
             if(a[i].at(ii)->evid()==9) {
               prob.rate_rm(this_cmtn, a[i].at(ii)->rate());
               a[i].erase(a[i].begin() + ii);
               --n_inf;
+              --n_end;
+              --ii;
             }
           }
         }
