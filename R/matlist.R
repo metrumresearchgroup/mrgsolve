471fa3370668a744971bfa9f36df9942841ab8ca
Kyle Baron <kyleb@metrumrg.com>
2021-05-08 07:57:51 -0500

    fix example

--- R/matlist.R
+++ R/matlist.R
@@ -218,7 +218,7 @@ setMethod("smat", "mrgsims", function(.x,make=FALSE,...) {
 #' revar(mod)
 #' mod <- zero_re(mod)
 #' revar(mod)
-#' revar(zero_re(omega))
+#' revar(zero_re(mod, omega))
 #' 
 #' @md
 #' @export
