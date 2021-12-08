198d113316a9cafbc179464657eb8ec64f4a3fa5
Kyle Baron <kyleb@metrumrg.com>
2021-12-08 13:51:19 -0600

    update nm-like; add functional test with autodec + nm-vars

--- inst/models/nm-like.cpp
+++ inst/models/nm-like.cpp
@@ -3,7 +3,7 @@ $PROB Model written with some nonmem-like syntax features
 $PLUGIN nm-vars autodec
 
 $PARAM
-THETA1 = 1, THETA2 = 21, THETA3 = 1.3, WT = 70, F1I = 0.5
+THETA1 = 1, THETA2 = 21, THETA3 = 1.3, WT = 70, F1I = 0.5, D2I = 2
 KIN = 100, KOUT = 0.1, IC50 = 10, IMAX = 0.9
 
 $CMT @number 3
@@ -14,6 +14,7 @@ V  = THETA(2);
 KA = THETA(3);
 
 F1 = F1I;
+D2 = D2I;
 A_0(3) = KIN / KOUT;
 
 $DES 
