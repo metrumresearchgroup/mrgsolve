a18c65b79c49ba35b216a0b5df9424fa946a92b1
Kyle Baron <kyleb@metrumrg.com>
2021-12-21 08:55:47 -0600

    point to base is.numeric

--- R/events.R
+++ R/events.R
@@ -578,7 +578,7 @@ ev_seq <- function(..., ID = NULL, .dots = NULL, id = NULL) {
   ii <- 0
   for(i in seq_along(out)) {
     this_name <- names(evs)[i]
-    spacer <- is.numeric(evs[[i]]) && length(evs[[i]])==1
+    spacer <- base::is.numeric(evs[[i]]) && length(evs[[i]])==1
     # TODO: deprecate .ii
     if(this_name == ".ii" & spacer) {
       deprecate_warn(
@@ -611,7 +611,7 @@ ev_seq <- function(..., ID = NULL, .dots = NULL, id = NULL) {
       )
       stop(err)
     }
-    if(!inherits(evs[[i]], "ev")) {
+    if(!is.ev(evs[[i]])) {
       err <- c(
         "\n",
         "found object with class: ", 
