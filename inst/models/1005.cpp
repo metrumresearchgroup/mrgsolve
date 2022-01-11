b206f032b65cf87987c9deff3720adb5de6138d0
Kyle Baron <kyleb@metrumrg.com>
2022-01-10 23:23:48 -0600

    nonmem-ize 1005

--- inst/models/1005.cpp
+++ inst/models/1005.cpp
@@ -3,11 +3,15 @@
 
 Run 
 
-file.show(system.file("nonmem", "1005", "1005.ctl",package="mrgsolve")) 
+file.show(system.file("nonmem", "1005", "1005.ctl", package = "mrgsolve")) 
 
 for equivalent NONMEM control stream
 
-[ PKMODEL ] cmt = "GUT CENT PERIPH", depot = TRUE
+[ PLUGIN ] autodec nm-vars
+  
+[ CMT ] @number 3
+  
+[ PKMODEL ] ncmt = 2, depot = TRUE
 
 [ PARAM ] SEX = 0, WT = 70
 
@@ -15,18 +19,18 @@ for equivalent NONMEM control stream
 project = system.file("nonmem", package = "mrgsolve")
 run = 1005
 
-[ MAIN ] 
-double CL=THETA1*exp(ETA(1)) * pow(THETA6,SEX) * pow(WT/70.0,THETA7);
-double V2=THETA2*exp(ETA(2));
-double KA=THETA3*exp(ETA(3));
-double Q =THETA4;
-double V3=THETA5;
-double S2=V2;
+[ PK ] 
+CL = THETA(1)*exp(ETA(1)) * pow(THETA(6),SEX) * pow(WT/70.0,THETA(7));
+V2 = THETA(2)*exp(ETA(2));
+KA = THETA(3)*exp(ETA(3));
+Q  = THETA(4);
+V3 = THETA(5);
+S2 = V2;
 
-[ TABLE ] 
-double F = CENT/S2;
-double Y=F*(1+EPS(1)) + EPS(2); 
-capture IPRED=F; 
+[ ERROR ] 
+F = A2/S2;
+Y = F*(1+EPS(1)) + EPS(2); 
+IPRED = F; 
 
 [ CAPTURE ]
 CL Q V2 V3 KA ETA(1) ETA(2) ETA(3) IPRED
