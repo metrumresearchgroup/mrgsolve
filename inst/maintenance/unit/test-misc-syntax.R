e62cb1bf63a84ab9bee35241f478ec26a9cc478c
Kyle Baron <kyleb@metrumrg.com>
2022-01-10 23:03:48 -0600

    additional tests - grepping out rendered code

--- inst/maintenance/unit/test-misc-syntax.R
+++ inst/maintenance/unit/test-misc-syntax.R
@@ -47,3 +47,132 @@ test_that("autodec + nm-vars using nm reserved", {
     regexp="reserved: A", fixed = TRUE
   )
 })
+
+test_that("grep code autodec and nm-vars", {
+  code <- '
+$PLUGIN autodec nm-vars
+$PARAM CL = 1, BC = 2
+$CMT FOO BAR YAK
+$MAIN 
+F1 = 0.25;
+X = 33;
+double Y = 44;
+ALAG2 = 5;
+D3 = 10;
+F1 = 1.23;
+$ODE @!audit
+DADT(1) = 1; 
+DADT(2) = 2;
+$TABLE
+Z = 5;
+'
+  
+  x <- mcode("parse1", code, compile = FALSE)
+  where <- x@soloc
+  f <- list.files(where, full.names = TRUE)
+  code1 <- readLines(f[1])
+  
+  f1 <- grepl("define F1", code1, fixed = TRUE)
+  al <- grepl("define ALAG", code1, fixed = TRUE)
+  d3 <- grepl("define D3", code1, fixed = TRUE)
+  x <- grepl("double X", code1, fixed = TRUE)
+  y <- grepl("double Y", code1, fixed = TRUE)
+  z <- grepl("double Z", code1, fixed = TRUE)
+  da <- grepl("DADT", code1, fixed = TRUE)
+  cl <- grepl("define CL", code1, fixed = TRUE)
+  
+  expect_equal(sum(f1), 1)
+  expect_equal(sum(al), 1)
+  expect_equal(sum(d3), 1)
+  expect_equal(sum(x), 1)
+  expect_equal(sum(y), 1)
+  expect_equal(sum(z), 1)
+  expect_equal(sum(da), 3)
+  expect_equal(sum(cl), 1)
+})
+
+test_that("grep code autodec", {
+  code <- '
+$PLUGIN autodec
+$PARAM CL = 1, BC = 2
+$CMT FOO BAR YAK
+$MAIN 
+F1 = 0.25;
+X = 33;
+double Y = 44;
+ALAG2 = 5;
+D3 = 10;
+$ODE @!audit
+DADT(1) = 1; 
+DADT(2) = 2;
+$TABLE
+Z = 5;
+'
+  
+  x <- mcode("parse2", code, compile = FALSE)
+  where <- x@soloc
+  f <- list.files(where, full.names = TRUE)
+  code1 <- readLines(f[1])
+  f1 <- grepl("double F1", code1, fixed = TRUE)
+  al <- grepl("double ALAG", code1, fixed = TRUE)
+  d3 <- grepl("double D3", code1, fixed = TRUE)
+  x <- grepl("double X", code1, fixed = TRUE)
+  y <- grepl("double Y", code1, fixed = TRUE)
+  z <- grepl("double Z", code1, fixed = TRUE)
+  da <- grepl("\\bDADT", code1, fixed = TRUE)
+  cl <- grepl("define CL", code1, fixed = TRUE)
+  
+  expect_equal(sum(f1), 1)
+  expect_equal(sum(al), 1)
+  expect_equal(sum(d3), 1)
+  expect_equal(sum(x), 1)
+  expect_equal(sum(y), 1)
+  expect_equal(sum(z), 1)
+  expect_equal(sum(da), 0)
+  expect_equal(sum(cl), 1)
+})
+
+test_that("grep code nm-vars", {
+  code <- '
+$PLUGIN  nm-vars
+$PARAM CL = 1, BC = 2
+$CMT FOO BAR YAK
+$MAIN 
+F1 = 0.25;
+double X = 33;
+double Y = 44;
+ALAG2 = 5;
+D3 = 10;
+F1 = 1.23;
+$ODE @!audit
+DADT(1) = 1; 
+DADT(2) = 2;
+$TABLE
+double Z = 5;
+'
+  
+  x <- mcode("parse3", code, compile = FALSE)
+  where <- x@soloc
+  f <- list.files(where, full.names = TRUE)
+  code1 <- readLines(f[1])
+  
+  f1 <- grepl("define F1", code1, fixed = TRUE)
+  al <- grepl("define ALAG", code1, fixed = TRUE)
+  d3 <- grepl("define D3", code1, fixed = TRUE)
+  x <- grepl("double X", code1, fixed = TRUE)
+  y <- grepl("double Y", code1, fixed = TRUE)
+  z <- grepl("double Z", code1, fixed = TRUE)
+  da <- grepl("DADT", code1, fixed = TRUE)
+  cl <- grepl("define CL", code1, fixed = TRUE)
+  foo <- grepl("FOO", code1, fixed = TRUE)
+  
+  expect_equal(sum(f1), 1)
+  expect_equal(sum(al), 1)
+  expect_equal(sum(d3), 1)
+  expect_equal(sum(x), 1)
+  expect_equal(sum(y), 1)
+  expect_equal(sum(z), 1)
+  expect_equal(sum(da), 3)
+  expect_equal(sum(cl), 1)
+  expect_equal(sum(foo), 3)
+})
