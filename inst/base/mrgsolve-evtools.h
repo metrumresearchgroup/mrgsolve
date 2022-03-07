fc6e37304754bbffb6c80af444f2c65dcbe24f2f
Kyle Baron <kyleb@metrumrg.com>
2022-03-07 08:35:10 -0600

    let's hold on the other evtools functions for now

--- inst/base/mrgsolve-evtools.h
+++ inst/base/mrgsolve-evtools.h
@@ -4,34 +4,4 @@ namespace mrgsolve {
     void push(databox& self, mrg::evdata ev) {
       self.mevector.push_back(ev);  
     }
-    mrg::evdata ev(double time, int evid) {
-      mrg::evdata obj(time, evid);
-      return obj;
-    }
-    void bolus(databox& self, const double time, const double amt, 
-               const int cmt, bool now = false) {
-      mrg::evdata ev(time, 1); 
-      ev.now = now;
-      ev.cmt = cmt;
-      ev.amt = amt;
-      mrgsolve::evt::push(self, ev);
-    }
-    void infuse(databox& self, const double time, const double amt, 
-                const double rate, const int cmt, bool now = false) {
-      mrg::evdata ev(time, 1); 
-      ev.now = now;
-      ev.cmt = cmt;
-      ev.amt = amt;
-      ev.rate = rate;
-      mrgsolve::evt::push(self, ev);
-    }
-    void replace(databox& self, const double time, const double amt, 
-                 const int cmt, bool now = false) {
-      mrg::evdata ev(time, 8); 
-      ev.now = now; 
-      ev.cmt = cmt; 
-      ev.amt = amt;
-      mrgsolve::evt::push(self, ev);
-    }
-  }
 }
