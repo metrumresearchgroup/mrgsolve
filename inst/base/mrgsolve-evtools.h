627a7dc84d1a5f4ae9783eb1407a5e4aeaf8679f
Kyle Baron <kyleb@metrumrg.com>
2022-03-12 17:17:35 -0600

    revert evt refactor

--- inst/base/mrgsolve-evtools.h
+++ inst/base/mrgsolve-evtools.h
@@ -1,5 +1,7 @@
 
-namespace evt {
-void push(databox& self, mrg::evdata& ev) {
-  self.mevector.push_back(ev);  
+namespace mrgsolve {
+  namespace evt {
+    void push(databox& self, mrg::evdata ev) {
+      self.mevector.push_back(ev);  
+    }
 }
