8ee3bd04570d1d7d1e21c8c8916b8932ae038686
Kyle Baron <kyleb@metrumrg.com>
2023-02-25 20:51:12 -0600

    Revert "Revert "Bulk capture of ETAs""

--- inst/maintenance/unit/test-capture.R
+++ inst/maintenance/unit/test-capture.R
@@ -70,7 +70,7 @@ test_that("capture via mread [SLV-TEST-0008]", {
     msg = "all requested `capture` variables must exist in the model"
   )
   mod <- mcode("capture-mread", code, capture="(everything)", compile = FALSE)
-  res <- c("CL","VP", "Q", "V3", "KA", "OGA2", "ETA_1", "ETA_2", "z", "b")
+  res <- c("CL", "VP", "Q", "V3", "KA", "OGA2", "ETA_1", "ETA_2", "z", "b")
   expect_equal(outvars(mod)$capture, res)
 })
 
@@ -93,4 +93,50 @@ test_that("dynamic capture under nm-vars [SLV-TEST-0009]", {
 test_that("capture pp directive via mread [SLV-TEST-0010]", {
   mod <- modlib("irm3", capture = "STIM", compile = FALSE)  
   expect_equal(outvars(mod)$capture, c("CP", "STIM"))
+})
+
+test_that("capture with @etas directive", {
+  base <- "$OMEGA 1 1 1 1 1\n$ENV asdk = seq(1,3)\n$CAPTURE @etas "
+  
+  code <- paste0(base, "1:2")
+  mod <- mcode("capture-at-etas-1", code, compile = FALSE)
+  expect_equal(mod@capture,   c(`ETA(1)` = "ETA1", `ETA(2)` = "ETA2"))
+  
+  code <- paste0(base, "c(1,2)")
+  mod2 <- mcode("capture-at-etas-1b", code, compile = FALSE)
+  expect_identical(mod@capture, mod2@capture)
+  
+  code <- paste0(base, "asdk")
+  mod <- mcode("capture-at-etas-1c", code, compile = FALSE)
+  expect_equal(mod@capture,   c(`ETA(1)` = "ETA1", `ETA(2)` = "ETA2", 
+                                `ETA(3)` = "ETA3"))
+  
+  expect_error(
+    mcode("capture-at-etas-2", base),
+    regexp = "must be text, not a logical value"
+  )
+  
+  code <- paste0(base, "foobar")
+  expect_error(
+    mcode("capture-at-etas-3", code), 
+    regexp = "could not parse this expression"
+  )
+  
+  code <- paste0(base, "1:100")
+  expect_error(
+    mcode("capture-at-etas-4", code), 
+    regexp = "must be integers between 1 and 5"
+  )
+  
+  code <- paste0(base, "letters")
+  expect_error(
+    mcode("capture-at-etas-5", code), 
+    regexp = "must resolve to an integer value"
+  )
+  
+  code <- paste0(base, "integer(0)")
+  expect_error(
+    mcode("capture-at-etas-7", code), 
+    regexp = "has length 0"
+  )
 })
