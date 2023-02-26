68cb9f26fe8e62835a6aba535589fb9aaa6ef357
Kyle Baron <kyleb@metrumrg.com>
2023-02-25 20:45:37 -0600

    Revert "Bulk capture of ETAs"

--- R/handle_spec_block.R
+++ R/handle_spec_block.R
@@ -357,8 +357,8 @@ handle_spec_block.specCAPTURE <- function(x, ...) {
 }
 
 #' @rdname BLOCK_PARSE
-CAPTURE <- function(x, env, pos = 1, annotated = FALSE, 
-                    etas = NULL, ...) {
+CAPTURE <- function(x, env, pos = 1, annotated = FALSE, ...) {
+  
   if(annotated) {
     context <- env[["incoming_names"]][pos]
     context <- glue("parse annotated capture block ({context})")
@@ -372,14 +372,7 @@ CAPTURE <- function(x, env, pos = 1, annotated = FALSE,
     x <- cvec_cs(x)
   }
   
-  if(!is.null(etas)) {
-    if(is.logical(etas)) {
-      abort("`etas` must be text, not a logical value.")  
-    }
-    env[["capture_etas"]] <- c(env[["capture_etas"]], etas)
-  } else {
-    check_block_data(x, env, pos)
-  }
+  check_block_data(x, env, pos)
   
   env[["capture"]][[pos]] <- x
   
