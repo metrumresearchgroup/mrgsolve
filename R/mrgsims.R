39df8177ec6c8219681455ab0d722babd488a081
Kyle Baron <kylebtwin@imap.cc>
2023-03-08 22:11:27 -0600

    fix generics

--- R/mrgsims.R
+++ R/mrgsims.R
@@ -235,29 +235,18 @@ slice.mrgsims <- function(.data,...) {
 }
 
 ##' @rdname mrgsims_dplyr
-##' @param .data_ mrgsims object
 ##' @export
-as_data_frame.mrgsims <- function(.data_,...) {
-  as_tibble(as.data.frame(.data_),...)
+as_data_frame.mrgsims <- function(x,...) {
+  as_tibble(as.data.frame(x),...)
 }
 
-#' @param .rows passed to [tibble::as_tibble()]
-#' @param .name_repair passed to [tibble::as_tibble()]
-#' @param rownames passed to [tibble::as_tibble()]
+
+#' @param x mrgsims object.
+#' 
 #' @rdname mrgsims_dplyr
 #' @export
-as_tibble.mrgsims <- function(x, 
-                              .rows = NULL, 
-                              .name_repair = c("check_unique", "unique", 
-                                               "universal", "minimal"), 
-                              rownames = NULL, ...) {
-  as_tibble(
-    as.data.frame(x), 
-    .rows = .rows, 
-    .name_repair = .name_repair, 
-    rownames = rownames, 
-    ...
-  )  
+as_tibble.mrgsims <- function(x, ...) {
+  as_tibble(as.data.frame(x), ...)  
 }
 
 #' @rdname mrgsims_dplyr
