e2aff468e533c30c1c12ab07acfba805399e9e71
Kyle Baron <kylebtwin@imap.cc>
2023-04-11 21:22:45 -0500

    inputs are stored with param_tags

--- R/mread.R
+++ R/mread.R
@@ -449,7 +449,6 @@ mread <- function(model, project = getOption("mrgsolve.project", getwd()),
   # lock some of this down so we can check order later
   x@code <- readLines(build[["modfile"]], warn=FALSE)
   x@shlib[["covariates"]] <- mread.env[["covariates"]]
-  x@shlib[["input"]] <- mread.env[["input"]]
   x@shlib[["param_tag"]] <- mread.env[["param_tag"]]
   x@shlib[["cpp_variables"]] <- build$cpp_variables
   inc <- spec[["INCLUDE"]]
