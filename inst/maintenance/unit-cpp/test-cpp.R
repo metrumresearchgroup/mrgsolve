7f1522c4d3bab5bf3e0b9d0e36d27102a1884285
Kyle Baron <kylebtwin@imap.cc>
2023-09-30 15:19:45 -0500

    test for hx reset

--- inst/maintenance/unit-cpp/test-cpp.R
+++ inst/maintenance/unit-cpp/test-cpp.R
@@ -29,52 +29,14 @@ $PK
 if(NEWIND <= 1) capture FLAG = 0;
 $CMT A
 $TABLE 
-if(TIME==2) {
-  self.mevent(2.001,33);
-  self.mevent(2.001,33);
-}
+if(TIME==2) self.mevent(2.001,33);
 if(EVID==33) ++FLAG;
 '
-
-mod <- mcode("test-cpp-re-event", code, end = -1, add = c(1,2,3))
-
-mrgsim(mod, ev(amt = 0, ID = 1:2, evid = 2)) %>% as.data.frame()
-
-
-
-code <- '
-
-$MAIN 
-if(NEWIND == 0) { 
-  int count = 0;
-}
-if(NEWIND <= 1) {
-  int dosen = 0;
-  double dose = 0;
-}
-
-$TABLE
-if(EVID> 30 && dosen < 11) {
-    if(dosen <= 1) dose = 100;
-    if(dosen==2)   dose = 200;  
-    if(dosen ==3)  dose = 300;
-    if(dosen > 3)  dose = 400;
-    if(dosen > 5)  dose =   0;
-    mrg::evdata ev(TIME,1);
-    ev.cmt = 1;
-    ev.amt = dose;
-    ev.now = true;
-    self.mevector.push_back(ev);
-    ++dosen;
-    self.mevent(TIME + 3, 33);
-}
-
-$CMT A
-$CAPTURE dosen dose
-'
-
-mod <- mcode("foo", code)
-
-data <- expand.ev(amt = 0, evid = 33, time = 0, ID = 1:2)
-
-mrgsim(mod, data = data, end = 33, delta = 0.1)  %>% plot()
+test_that("ev history is reset", {
+  mod <- mcode("test-cpp-re-event", code, end = -1, add = c(1,2,3))
+  out <- mrgsim_df(mod, ev(amt = 0, ID = 1:2, evid = 2)) 
+  expect_equal(sum(out$FLAG), 2)  
+  out3 <- subset(out, time==3)
+  expect_length(out3$FLAG, 2)
+  expect_true(all(out3$FLAG==1))
+})
