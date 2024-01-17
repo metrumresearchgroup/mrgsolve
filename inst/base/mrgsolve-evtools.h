3d3d18a814e09f303f021648e43ab9610b5438b3
Kyle Baron <kyleb@metrumrg.com>
2024-01-16 22:45:55 -0600

    restore mrgsolve::evt::push

--- inst/base/mrgsolve-evtools.h
+++ inst/base/mrgsolve-evtools.h
@@ -51,3 +51,11 @@ void push(databox& self, mrgsolve::evdata ev) {
 }
 
 }
+
+namespace mrgsolve {
+namespace evt {
+void push(databox& self, mrg::evdata ev) {
+  self.mevector.push_back(ev);  
+}
+}
+}
