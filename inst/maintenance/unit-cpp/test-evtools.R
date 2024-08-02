bf2064778d9aa3c3ccd9e0d30f6ecef3424fd4f8
Kyle Baron <kyleb@metrumrg.com>
2024-08-01 23:03:31 -0500

    add tests

--- inst/maintenance/unit-cpp/test-evtools.R
+++ inst/maintenance/unit-cpp/test-evtools.R
@@ -13,16 +13,19 @@ $SET end = 12, rtol = 1e-4, atol = 1e-4, delta = 0.25
 $PLUGIN evtools
 $PARAM 
 mode = 0, f1 = 1, dose = 100, irate = 50, newtime = 2
-reptime = 5
+reptime = 5, A0 = 0, B0 = 0, C0 = 0, dosetime = 0
 $CMT A B C
 $PK
 F_A = f1;
+A_0 = A0;
+B_0 = B0;
+
 $DES
 dxdt_A = -1 * A;
 dxdt_B =  1 * A - 0.1 * B;
 dxdt_C = 0;
 $TABLE
-bool givedose = TIME==0;
+bool givedose = TIME==dosetime;
 if(mode==1 && givedose) {
   evt::bolus(self, dose, 1);
 }
@@ -58,11 +61,31 @@ if(mode==8 && givedose) {
   evt::retime(rep, reptime);
   self.push(rep);
 }
+if(mode==9 && givedose) {
+  evt::reset(self);
+}
+if(mode==10 && givedose) {
+  evt::ev res = evt::reset();
+  res.time = 15;
+  self.push(res);
+}
+if(mode==11 && givedose) {
+  evt::reset(self, 100, 3);
+}
+if(mode==12 && givedose) {
+  evt::reset(self, 100, 1, 20);
+}
+if(mode==13 && givedose) {
+  evt::ev res = evt::reset(100, 3);
+  self.push(res);
+}
+if(mode==14 && givedose) {
+  evt::ev res = evt::reset(100, 1, 20);
+  self.push(res);
+}
 '
 
 mod <- mcode("test-evtools-model-1", code)
-
-mrgsim(mod, param = list(mode = 1, f1 = 1)) 
 
 test_that("evtools - bolus now", {
   
@@ -137,4 +160,55 @@ test_that("evtools - replace", {
   expect_true(all(before$C==100))
   after <- filter(c, time >= 8) # note: >= 8
   expect_true(all(after$C==10))
+})
+
+test_that("evtools - reset", {
+  mod <- param(mod, dosetime = 5, B0 = 50, mode = 9)
+  out <- mrgsim(mod)
+  expect_true(all(out$A==0))
+  expect_true(all(out$C==0))
+  x <- filter(out, time %in% c(0,5))
+  expect_true(all(x$B==50))
+  x <- filter(out, time==4)
+  expect_equal(x$B, 50*exp(-0.1*4), tolerance = 1e-2)
+  mod <- param(mod, mode = 10)
+  out2 <- mrgsim(mod)
+  expect_equal(out@data, out2@data)
+})
+
+test_that("evtools - reset with bolus", {
+  mod <- param(mod, dosetime = 5, B0 = 50, mode = 11)
+  out <- mrgsim(mod)
+  x <- filter(out, time==5)
+  expect_equal(x$A, 0)
+  expect_equal(x$B, 50)
+  expect_equal(x$C, 100)
+  x <- filter(out, time==4.75)
+  expect_equal(x$C, 0)
+  x <- filter(out, time > 5)
+  expect_true(all(x$C==100))
+})
+
+
+test_that("evtools - reset with infusion", {
+  mod <- param(mod, dosetime = 5, B0 = 50, mode = 12)
+  out <- mrgsim(mod, rtol = 1e-12)
+  x <- filter(out, time %in% c(0,5))
+  expect_true(all(x$B==50))
+  expect_true(all(x$A==0))
+  x <- filter(out, time == 4.75)
+  expect_equal(x$A, 0)
+  expect_equal(x$B, 50*exp(-0.1*4.75), tolerance = 1e-2)
+  # Maximum A is at the end of the infusion
+  x <- filter(out, A==max(A))
+  expect_equal(x$time, 10)
+  
+  # Identical results
+  out1 <- mrgsim(mod, param = list(mode = 11))
+  out2 <- mrgsim(mod, param = list(mode = 12))
+  out3 <- mrgsim(mod, param = list(mode = 13))
+  out4 <- mrgsim(mod, param = list(mode = 14))
+  
+  expect_identical(out1@data, out3@data)
+  expect_identical(out2@data, out4@data)
 })
