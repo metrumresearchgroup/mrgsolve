f99929064452ca10dd38ab7a359df71a526f541f
Kyle Baron <kyleb@metrumrg.com>
2024-09-01 18:26:28 -0500

    Update tests to include a 'later' variant

--- inst/maintenance/unit-cpp/test-evtools-dosing.R
+++ inst/maintenance/unit-cpp/test-evtools-dosing.R
@@ -23,6 +23,7 @@ Amt   =  100
 Cmt2  = -1
 Amt2  = -1
 Rate2 = -1
+Time2 = -1
 
 $CMT B D
 
@@ -44,6 +45,7 @@ if(TIME==0 && Mdose > 0) {
   if(Rate2 > 0) evt::rate(dose, Rate2);
   if(Cmt2 > 0) evt::cmt(dose, Cmt2);
   if(Amt2 > 0) evt::amt(dose, Amt2);
+  if(Time2 > 0) evt::retime(dose, Time2);
   self.push(dose);
 }
 '
@@ -58,6 +60,14 @@ test_that("Bolus - ss and addl via evt", {
   out2 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
   
   expect_identical(out1$B, out2$B)
+  
+  # same dose, but "later"
+  d <-   ev(amt = 100, ii = 24, addl = 3, time = 3)
+  p <- list(Amt = 100, Ii = 24, Addl = 3, Time2 = 3)
+  out3 <- mrgsim(mod1, d, recsort = 3, obsonly = TRUE)
+  out4 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
+  
+  expect_identical(out3$B, out4$B)
 })
 
 test_that("Bolus, lag - ss and addl via evt", {
@@ -67,6 +77,14 @@ test_that("Bolus, lag - ss and addl via evt", {
   out2 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
   
   expect_identical(out1$B, out2$B)
+  
+  # same dose, but "later"
+  d <-   ev(amt = 100, ii = 24, addl = 3, Lag = 5, time = 2.5)
+  p <- list(Amt = 100, Ii = 24, Addl = 3, Lag = 5, Time2 = 2.5)
+  out3 <- mrgsim(mod1, d, recsort = 3, obsonly = TRUE)
+  out4 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
+
+  expect_identical(out3$B, out4$B)
 })
 
 test_that("Bolus, ss - ss and addl via evt", {
@@ -76,6 +94,14 @@ test_that("Bolus, ss - ss and addl via evt", {
   out2 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
   
   expect_identical(out1$B, out2$B)
+  
+  # same dose, but "later"
+  d <-   ev(amt = 100, ii = 24, addl = 3, ss = 1, time = 3.5)
+  p <- list(Amt = 100, Ii = 24, Addl = 3, Ss = 1, Time2 = 3.5)
+  out3 <- mrgsim(mod1, d, recsort = 3, obsonly = TRUE)
+  out4 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
+  
+  expect_identical(out3$B, out4$B)
 })
 
 test_that("Bolus, ss, lag - ss and addl via evt", {
@@ -85,6 +111,14 @@ test_that("Bolus, ss, lag - ss and addl via evt", {
   out2 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
   
   expect_identical(out1$B, out2$B)
+  
+  # same dose, but "later"
+  d <-   ev(amt = 100, ii = 24, addl = 3, ss = 1, Lag = 3, time = 3.1)
+  p <- list(Amt = 100, Ii = 24, Addl = 3, Ss = 1, Lag = 3, Time2 = 3.1)
+  out3 <- mrgsim(mod1, d, recsort = 3, obsonly = TRUE)
+  out4 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
+  
+  expect_identical(out3$B, out4$B)
 })
 
 test_that("Infusion - ss and addl via evt", {
@@ -94,6 +128,14 @@ test_that("Infusion - ss and addl via evt", {
   out2 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
   
   expect_identical(out1$B, out2$B)
+  
+  # same dose, but "later"
+  d <-   ev(amt = 100, ii = 12, addl = 3, Dur = 7, rate = -2, time = 2)
+  p <- list(Amt = 100, Ii = 12, Addl = 3, Dur = 7, Rate = -2, Time2 = 2)
+  out3 <- mrgsim(mod1, d, recsort = 3, obsonly = TRUE)
+  out4 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
+  
+  expect_identical(out3$B, out4$B)
 })
 
 test_that("Infusion, lag - ss and addl via evt", {
@@ -103,6 +145,14 @@ test_that("Infusion, lag - ss and addl via evt", {
   out2 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
   
   expect_identical(out1$B, out2$B)
+  
+  # same dose, but "later"
+  d <-   ev(amt = 100, ii = 8, addl = 3, Lag = 5, Dur = 5, rate = -2, time = 4)
+  p <- list(Amt = 100, Ii = 8, Addl = 3, Lag = 5, Dur = 5, Rate = -2, Time2 = 4)
+  out3 <- mrgsim(mod1, d, recsort = 3, obsonly = TRUE)
+  out4 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
+  
+  expect_identical(out3$B, out4$B)
 })
 
 test_that("Infusion, ss - ss and addl via evt", {
@@ -112,6 +162,14 @@ test_that("Infusion, ss - ss and addl via evt", {
   out2 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
   
   expect_identical(out1$B, out2$B)
+  
+  # same dose, but "later"
+  d <-   ev(amt = 100, ii = 24, addl = 2, ss = 1, Dur = 8, rate = -2, time = 1.5)
+  p <- list(Amt = 100, Ii = 24, Addl = 2, Ss = 1, Dur = 8, Rate = -2, Time2 = 1.5)
+  out3 <- mrgsim(mod1, d, recsort = 3, obsonly = TRUE)
+  out4 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
+  
+  expect_identical(out3$B, out4$B)
 })
 
 test_that("Infusion, ss, lag - ss and addl via evt", {
@@ -121,6 +179,16 @@ test_that("Infusion, ss, lag - ss and addl via evt", {
   out2 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
   
   expect_identical(out1$B, out2$B)
+  
+  # same dose, but "later"
+  d <-   ev(amt = 100, ii = 16, addl = 3, ss = 1, Lag = 3, Dur = 9, rate = -2, 
+            time = 2.1)
+  p <- list(Amt = 100, Ii = 16, Addl = 3, Ss = 1, Lag = 3, Dur = 9, Rate = -2, 
+            Time2 = 2.1)
+  out3 <- mrgsim(mod1, d, recsort = 3, obsonly = TRUE)
+  out4 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
+  
+  expect_identical(out3$B, out4$B)
 })
 
 test_that("Alternate infusion - ss and addl via evt", {
@@ -139,6 +207,13 @@ test_that("Single dose - dosing via evt", {
   out2 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
   
   expect_identical(out1$B, out2$B)
+  
+  d <-   ev(amt = 100, ii = 24, tinf = 3, time = 3.3)
+  p <- list(Amt = 100, Ii = 24, Addl = 0, Dur = 3, Rate = -2, Time2 = 3.3)
+  out3 <- mrgsim(mod1, d, recsort = 3, obsonly = TRUE)
+  out4 <- mrgsim(mod2, param = p, obsonly = TRUE, recsort = 3)
+  
+  expect_identical(out3$B, out4$B)
 })
 
 test_that("Switch compartment - dosing via evt", {
