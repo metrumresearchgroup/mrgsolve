99160c901b4bfa696facf26a6a97e30649543188
Kyle Baron <kyleb@metrumrg.com>
2024-09-12 07:12:58 -0500

    tests for event blocjk

---
+++ inst/maintenance/unit-cpp/test-event-block.R
@@ -0,0 +1,103 @@
+library(testthat)
+library(mrgsolve)
+library(dplyr)
+
+Sys.setenv(R_TESTS="")
+
+options("mrgsolve_mread_quiet"=TRUE)
+
+local_edition(3)
+
+code_event <- '
+$PLUGIN evtools nm-vars autodec
+
+$PARAM 
+Ii    =  24
+Ss    =  1
+Addl  =  3
+Amt   =  100
+Rate  =  20
+CL    = 2
+V     = 20
+
+$PKMODEL cmt = "B"
+
+$EVENT
+if(TIME==0) {
+  evt::ev dose = evt::infuse(Amt, 1, Rate); 
+  evt::ss(dose, Ss); 
+  evt::ii(dose, Ii); 
+  evt::addl(dose, Addl); 
+  self.push(dose);
+}
+capture b = A(1)/V;
+capture c = 1.23;
+double d = 50;
+
+$ERROR
+capture cp = B/V;
+e = d/2;
+
+$CAPTURE e d
+'
+
+code_error <- '
+$PLUGIN evtools nm-vars autodec
+
+$PARAM 
+Ii    =  24
+Ss    =  1
+Addl  =  3
+Amt   =  100
+Rate  =  20
+CL    = 2
+V     = 20
+
+$PKMODEL cmt = "B"
+
+$ERROR
+
+if(TIME==0) {
+  evt::ev dose = evt::infuse(Amt, 1, Rate); 
+  evt::ss(dose, Ss); 
+  evt::ii(dose, Ii); 
+  evt::addl(dose, Addl); 
+  self.push(dose);
+}
+capture cp = B/V;
+'
+
+
+mod1 <- mcode("event", code_event, delta = 0.1, end = 120)
+mod2 <- mcode("error", code_error, delta = 0.1, end = 120)
+out1 <- mrgsim(mod1)
+out2 <- mrgsim(mod2)
+
+test_that("$EVENT result matches $TABLE results", {
+  expect_identical(out1$B, out2$B)
+  # Known that cp isn't calculated when $TABLE is used
+  expect_equal(out2$cp[1], 0)
+  # Using $EVENT allows this to be calculated
+  expect_equal(out1$cp[1], out1$B[1]/mod1$V)
+})
+
+test_that("declare inside $EVENT", {
+  expect_true(all(out1$d == 50))  
+  expect_true(all(out1$e == out1$d/2))
+})
+
+test_that("capture from $EVENT", {
+  expect_true(all(abs(out1$c - 1.23) < 1e-7)) 
+  expect_equal(out1$b, out2$cp)
+})
+
+test_that("check internals", {
+  f <- mrgsolve:::funset(mod1)
+  expect_equal(nrow(f$symbols), 5)
+  expect_true(all(f$symbols$loaded))
+  expect_equal(f$symbols$name[4], "_model_event_event__")
+  
+  p <- mrgsolve:::pointers(mod1)
+  expect_equal(length(p), 5)
+  expect_equal(names(p)[4], "event")
+})
