3acd1d0d437be3804e3c73ce4c5dc275f5d247b9
Kyle Baron <kyleb@metrumrg.com>
2025-03-08 08:39:35 -0600

    start R api for setting / updating

--- R/class_mrgmod.R
+++ R/class_mrgmod.R
@@ -871,3 +871,12 @@ outvars <- function(x, unlist = FALSE) {
   if(unlist) unlist(ans, use.names=FALSE)
   ans
 }
+
+#' Set tolerances on compartment-by-compartment basis
+#' 
+#' 
+#' 
+model_atol <- function(x, .object = NULL) {
+  
+}
+
