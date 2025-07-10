3882a3ce3666a7cb4b008a28b0bab9eccea26b8e
Kyle Baron <kyleb@metrumrg.com>
2025-07-10 15:34:57 -0500

    change argument order in actual calls, including Event

--- src/odeproblem.cpp
+++ src/odeproblem.cpp
@@ -242,12 +242,12 @@ void odeproblem::init_call_record(const double& time) {
 
 //! Call <code>$TABLE</code> function.
 void odeproblem::table_call() {
-  Table(Y,Init_value,Param,F,R,d,pred,Capture,simeps);  
+  Table(Init_value,Y,Param,F,R,d,pred,Capture,simeta,simeps);  
 }
 
 //! Call <code>$EVENT</code> function.
 void odeproblem::event_call() {
-  Event(Y,Init_value,Param,F,R,d,pred,Capture,simeps);  
+  Event(Init_value,Y,Param,F,R,d,pred,Capture,simeps);  
 }
 
 //! Call <code>$PREAMBLE</code> function.
