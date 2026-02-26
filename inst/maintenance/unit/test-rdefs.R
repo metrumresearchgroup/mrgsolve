08a941c77a1b63653a58a562cea31a54cdbc6078
Kyle Baron <kyleb@metrumrg.com>
2026-02-26 07:27:19 -0600

    tweak tests so they will work on gcc

--- inst/maintenance/unit/test-rdefs.R
+++ inst/maintenance/unit/test-rdefs.R
@@ -15,38 +15,38 @@ code <- c("$param B = 1", "$cmt A", "$omega @labels ETACL\n 1")
 test_that("Params are const everywhere", {
   codex <- c(code, "$preamble B = 2;")
   a <- mtry("param-preamble", codex, recover = TRUE)
-  expect_match(a$out$stderr, "declared const here",  all = FALSE)
+  expect_match(a$out$stderr, "B = 2",  all = FALSE)
 
   codex <- c(code, "$main B = 2;")
   a <- mtry("param-main", codex, recover = TRUE)
-  expect_match(a$out$stderr, "declared const here",  all = FALSE)
+  expect_match(a$out$stderr, "B = 2",  all = FALSE)
 
   codex <- c(code, "$ode B = 2;")
   a <- mtry("param-ode", codex, recover = TRUE)
-  expect_match(a$out$stderr, "declared const here",  all = FALSE)
+  expect_match(a$out$stderr, "B = 2",  all = FALSE)
 
   codex <- c(code, "$table B = 2;")
   a <- mtry("param-table", codex, recover = TRUE)
-  expect_match(a$out$stderr, "declared const here",  all = FALSE)
+  expect_match(a$out$stderr, "B = 2",  all = FALSE)
 
 })
 
 test_that("FRDA is const in TABLE", {
   codex <- c(code, "$main F_A = 0.9;", "$table F_A = 0.5;")
   a <- mtry("frda-table", codex, recover = TRUE)
-  expect_match(a$out$stderr, "declared const here",  all = FALSE)
+  expect_match(a$out$stderr, "F_A = 0.5",  all = FALSE)
 })
 
 test_that("ETA is const in MAIN", {
   codex <- c(code, "$main ETACL = 1.2;")
   a <- mtry("frda-table", codex, recover = TRUE)
-  expect_match(a$out$stderr, "cannot assign to variable",  all = FALSE)
+  expect_match(a$out$stderr, "ETACL = 1.2",  all = FALSE)
 })
 
 test_that("CMT_0 is const in TABLE", {
   codex <- c(code, "$table A_0 = 100;")
   a <- mtry("cmt-0-table", codex, recover = TRUE)
-  expect_match(a$out$stderr, "declared const here",  all = FALSE)
+  expect_match(a$out$stderr, "A_0 = 100",  all = FALSE)
 })
 
 test_that("FRDA is mutable in MAIN", {
