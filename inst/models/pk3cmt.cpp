1da2eb2ac424996bd0dfce763d998009f1dc3d10
Kyle Baron <kyleb@metrumrg.com>
2026-03-23 07:06:45 -0500

    Revert "update pk3cmt to be consistent in clearance and volume names"
    This reverts commit e34ba5672050d5c16d01dcb593cd831bc31537a7.

--- inst/models/pk3cmt.cpp
+++ inst/models/pk3cmt.cpp
@@ -9,11 +9,11 @@ $PROB
   
 $PARAM @annotated
 CL   :   1 : Clearance (volume/time)
-V2   :  20 : Central volume (volume)
-Q3   :   2 : First inter-compartmental clearance (volume/time)
-V3   :  10 : First peripheral volume (volume)
-Q4   : 0.5 : Second inter-compartmental clearance (volume/time)
-V4   :  50 : Second peripheral volume (volume) 
+VC   :  20 : Central volume (volume)
+Q    :   2 : First inter-compartmental clearance (volume/time)
+VP   :  10 : First peripheral volume (volume)
+Q2   :   2 : Second inter-compartmental clearance (volume/time)
+VP2  : 100 : Second peripheral volume (volume) 
 KA1  :   1 : Absorption rate constant 1 (1/time)
 KA2  :   1 : Absorption rate constant 2 (1/time)
 VMAX :   0 : Maximum velocity (mass/time)
@@ -22,22 +22,22 @@ KM   :   2 : Michaelis Constant (mass/volume)
 $CMT  @annotated
 EV1     : First extravascular compartment (mass)
 CENT    : Central compartment (mass)
-PERIPH1 : First peripheral compartment (mass) 
+PERIPH  : First peripheral compartment (mass) 
 PERIPH2 : Second peripheral compartment (mass)
 EV2     : Second extravascular compartment (mass)
   
 $GLOBAL
-#define CP (CENT/V2)
-#define CT (PERIPH1/V3)
-#define CT2 (PERIPH2/V4)
+#define CP (CENT/VC)
+#define CT (PERIPH/VP)
+#define CT2 (PERIPH2/VP2)
 #define CLNL (VMAX/(KM+CP))
 
 $ODE
 dxdt_EV1 = -KA1*EV1;
 dxdt_EV2 = -KA2*EV2;
-dxdt_CENT = KA1*EV1 + KA2*EV2 - (CL+CLNL+Q3+Q4)*CP  + Q3*CT + Q4*CT2;
-dxdt_PERIPH1 = Q3*CP - Q3*CT;
-dxdt_PERIPH2 = Q4*CP - Q4*CT2;
+dxdt_CENT = KA1*EV1 + KA2*EV2 - (CL+CLNL+Q+Q2)*CP  + Q*CT + Q2*CT2;
+dxdt_PERIPH = Q*CP - Q*CT;
+dxdt_PERIPH2 = Q2*CP - Q2*CT2;
 
 $CAPTURE @annotated 
 CP : Plasma concentration (mass/volume)
