03888431922525f6865864fea12e0bc7e7b90f08
Kyle Baron <kyleb@metrumrg.com>
2026-06-24 06:17:29 -0500

    remove () around nocb

--- src/devtran.cpp
+++ src/devtran.cpp
@@ -509,7 +509,7 @@ Rcpp::List DEVTRAN(const Rcpp::List parin,
       }
       
       // nocb we copy tto parameters just prior to $PK
-      if(dat.any_copy && (nocb)) {
+      if(dat.any_copy && nocb) {
         // will call lsoda_init if parameters are copied
         dat.copy_parameters_nocb(i, this_rec, &prob);
       }
